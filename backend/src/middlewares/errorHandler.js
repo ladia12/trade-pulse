01f28d917ffe9f3e41c71193ef632e0ff2127094
YASH <ladiayash@gmail.com>
2025-06-19 19:52:56 +0530

    Revert "Restructure backend code with proper project structure"
    This reverts commit acaf8ab13f0d9f91cdd2bd55c9acfabc958f18ba.

--- backend/src/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  console.error('Unhandled error:', error);
-  res.status(500).json({
-    status: 'error',
-    message: 'Internal server error'
-  });
-};
-
-module.exports = errorHandler;
--- backend/src/routes/analyze.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const analyzeController = require('../controllers/analyze.controller');
-
-const router = express.Router();
-
-router.post('/analyze', analyzeController.analyzeCompany);
-
-module.exports = router;
